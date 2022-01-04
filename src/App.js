9d65813f6d90a2ad73973d69286a19874ae86557
Caner <kurucaner@gmail.com>
2022-01-03 21:10:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const createDigits = () => {
+  const digits = [];
+  for (let i = 1; i < 10; i++) {
+    digits.push(<button key={i}>{i}</button>);
+  }
+  return digits;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="App">
+      <div classname="calculator">
+        <div classname="display">
+          <span>(0)</span> 0
+        </div>
+
+        <div classname="operators">
+          <button>+</button>
+          <button>-</button>
+          <button>*</button>
+          <button>/</button>
+
+          <button>DEL</button>
+        </div>
+
+        <div classname="digits">
+          {createDigits()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
